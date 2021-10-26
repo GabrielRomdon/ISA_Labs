9786c28d4397e7ac635e3c75c86f2e6a2b734312
simone-valente <isa14_2021_2022@localhost.localdomain>
2021-10-26 14:39:02 +0200

    new version of lookahead, seems working

---
+++ Lab1/Files/VHDL/look_ahead/src/pkgfla.vhd
@@ -0,0 +1,7 @@
+package pkgfla is
+	constant nb : integer := 14;
+	constant r : integer := 7; --5
+	constant nb_w : integer := 15; --17vers0
+	constant nb_a : integer := 14;	--22
+	constant nb_fb : integer := 7;
+end pkgfla;
