714dfe3b1aa8d695fc71f9da48a37cf6d511f12a
simone-valente <isa14_2021_2022@localhost.localdomain>
2021-11-02 16:36:11 +0100

    changed bidtwidth of ff from 6 to 7 to take into account corner cases

--- Lab1/Files/VHDL_synth/src/IIR_filter_template.vhd
+++ Lab1/Files/VHDL_synth/src/IIR_filter_template.vhd
@@ -27,7 +27,8 @@ architecture BEHAVIORAL of IIR_FILTER is
 
 signal w_past, w: std_logic_vector (nb downto 0);-- := (others => '0'); --the result of the sum could have a reminder that has to be taken into account
 signal fb_tmp, ff_tmp_0, ff_tmp_1  : std_logic_vector (nb*2 downto 0);-- := (others => '0');
-signal fb, ff : std_logic_vector (nb-r-2 downto 0);-- := (others => '0');
+signal fb : std_logic_vector (nb-r-2 downto 0);-- := (others => '0');
+signal ff : std_logic_vector (nb-r-1 downto 0);
 constant zero_padding : std_logic_vector (r-1 downto 0):= (others => '0') ;
 begin
 
@@ -36,7 +37,7 @@ begin
 			w <= std_logic_vector(signed(DIN(nb-1)& DIN) + signed(fb(nb-r-2) & fb)) when RST_n='1' else (others => '0'); --ovf can happen on nb bits, need nb+1!!
 			ff_tmp_0 <= std_logic_vector(signed(b0)*signed(w)) when RST_n='1' else (others => '0');
 			ff_tmp_1 <= std_logic_vector(signed(b1)*signed(w_past)) when RST_n='1' else (others => '0');
-			ff <= std_logic_vector(signed(ff_tmp_0(nb*2-3 downto nb*2-2-(nb-r)+1)) + signed(ff_tmp_1(nb*2-3 downto nb*2-2-(nb-r)+1))) when RST_n='1' else (others => '0'); --careful this could give ovf 7b+7b..NECESSARY TO MANAGE??SHOULD BE 8??
+			ff <= std_logic_vector(signed(ff_tmp_0(nb*2-2 downto nb*2-2-(nb-r)+1)) + signed(ff_tmp_1(nb*2-2 downto nb*2-2-(nb-r)+1))) when RST_n='1' else (others => '0'); --careful this could give ovf 7b+7b..NECESSARY TO MANAGE??SHOULD BE 8??
 			
 filter : process(clk)
 	
@@ -52,13 +53,11 @@ filter : process(clk)
 		elsif (VIN='1') then
 			--perform the task;normal operation of the filter
 			w_past <= w;
-			DOUT <=ff(nb-r-2) & ff & zero_padding;
+			DOUT <= ff & zero_padding;
 			VOUT <= '1';
 			
 		elsif (VIN='0') then 
  			VOUT <= '0';
-			--leave the output to previous value
-			--do nothing, leave VOUT to one or not??
 		end if;
 		
 	end if;
